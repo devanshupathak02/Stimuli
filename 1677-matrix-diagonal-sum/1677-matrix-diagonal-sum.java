41100948ee1d1137e1a11e26333ab47a50bcd9dd
DEVANSHU PATHAK <124905708+devanshupathak02@users.noreply.github.com>
2025-06-14 13:28:59 +0530

    Time: 0 ms (100%), Space: 44.7 MB (15.02%) - LeetHub

---
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum=0;
+        int len=mat.length;
+
+        for(int i=0;i<len;i++){
+            sum += mat[i][i] + mat[i][len-1-i];
+        }
+        if(len%2 !=0){
+            sum -= mat[len/2][len/2];
+        }
+        return sum;
+    }
+}
