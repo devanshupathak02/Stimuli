12347e8efbaa85841e5ddb8ff61d4052f4f53377
DEVANSHU PATHAK <124905708+devanshupathak02@users.noreply.github.com>
2025-06-23 13:10:40 +0530

    Time: 9 ms (17.88%), Space: 40.9 MB (83.99%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++){
+            n-=i;
+            int rem= n;
+            count++;
+            if(rem < i){
+                break;
+            }
+            
+        }
+        return count;
+    }
+}
