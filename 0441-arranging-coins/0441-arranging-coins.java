accb9ff0e698a8dd375b5835065cbf47bc028e6b
DEVANSHU PATHAK <124905708+devanshupathak02@users.noreply.github.com>
2025-06-24 23:21:41 +0530

    Time: 1 ms (98.16%), Space: 41 MB (54.39%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,15 +1,21 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int count=0;
-        for(int i=1;i<=n;i++){
-            n-=i;
-            int rem= n;
-            count++;
-            if(rem < i){
-                break;
+       int s=1;
+       int e=n;
+     //   1 2 3 4 5 6 7 8 9
+        while(s <= e){
+            long mid= s + (e-s)/2;
+            long sum= mid *(mid+1)/2 ;//sum of k element
+
+            if(sum== n){
+                return (int)mid;
             }
-            
+            else if(sum >n){
+                e=(int)mid-1;
+            }else{
+                s=(int)mid+1;
+            }
         }
-        return count;
+        return e;
     }
 }
